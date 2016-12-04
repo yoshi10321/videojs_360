e745a9b8af92c43b34c0a87aef97693a5c8ae73c
Mitsui Yoshito <pooh.aflac@gmail.com>
2016-12-04 17:20:14 +0900

    add video player resize

--- js/index.js
+++ js/index.js
@@ -1,5 +1,10 @@
 // var player = videojs('example-video');
-var player = videojs('videojs-panorama-player');
+var player = videojs('videojs-panorama-player', {}, function () {
+    window.addEventListener("resize", function () {
+        var canvas = player.getChild('Canvas');
+        canvas.handleResize();
+    });
+});
 
 // player.play();
 
